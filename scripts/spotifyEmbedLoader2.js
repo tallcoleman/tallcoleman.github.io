0eec4ed1fb0e67abf0aacf142b59217c86f9b110
Ben Coleman <ben.coleman@alum.utoronto.ca>
2023-03-18 16:46:00 -0400

    Added jekyll-minifier plugin

--- scripts/spotifyEmbedLoader2.js
+++ scripts/spotifyEmbedLoader2.js
@@ -1,18 +1,1 @@
-// replace placeholder link from spotifyembed.html include with proper spotify embed
-loadSpotifyEmbeds = function() {
-  const spotifyEmbeds = document.querySelectorAll('.spotify-embed');
-  const spotifyLinks = document.querySelectorAll('.spotify-no-embed-link');
-
-  for (let spotifyEmbed of spotifyEmbeds) {
-    let embedHTML = `<iframe id="iframe" src="https://open.spotify.com/embed/${spotifyEmbed.dataset.spotifytype}/${spotifyEmbed.dataset.spotifyuri}" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
-    spotifyEmbed.innerHTML = embedHTML;
-  }
-
-  for (let spotifyLink of spotifyLinks) {
-    spotifyLink.style.display="none";
-  }
-}
-
-// add event listener for embed loading function
-const spotifyEmbedButton = document.querySelector('.spotify-embed-button');
-spotifyEmbedButton.addEventListener('click', loadSpotifyEmbeds);
+loadSpotifyEmbeds=function(){const e=document.querySelectorAll(".spotify-embed"),t=document.querySelectorAll(".spotify-no-embed-link");for(let t of e){let e=`<iframe id="iframe" src="https://open.spotify.com/embed/${t.dataset.spotifytype}/${t.dataset.spotifyuri}" width="100%" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;t.innerHTML=e}for(let e of t)e.style.display="none"};const spotifyEmbedButton=document.querySelector(".spotify-embed-button");spotifyEmbedButton.addEventListener("click",loadSpotifyEmbeds);
